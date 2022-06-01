ab5a10ff2e78accc08c73a58a0e2dae5150340a2
GitHub Action <action@github.com>
2022-06-01 00:57:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/joeskj/contextualstryker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/joeskj/contextualstryker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.joeskj.contextualstryker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.joeskj.contextualstryker.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/joeskj/contextualstryker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/joeskj/contextualstryker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.joeskj.contextualstryker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.joeskj.contextualstryker.MyBundle
 
 class MyProjectService(project: Project) {
 
